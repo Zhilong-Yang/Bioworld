58452b3e8091b6f3f476bba37858f3b03469e055
zhilong yang <yzl_200@163.com>
2021-03-04 16:14:10 +0800

    json webapi

---
+++ src/Bioworld.WebApi/Formatters/JsonOutputFormatter.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Open.Serialization.Json;
+
+namespace Bioworld.WebApi.Formatters
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc.Formatters;
+
+    internal class JsonOutputFormatter:IOutputFormatter
+    {
+        private readonly IJsonSerializer _serializer;
+
+        public JsonOutputFormatter(IJsonSerializer serialize)
+        {
+            _serializer = serialize;
+        }
+
+        public bool CanWriteResult(OutputFormatterCanWriteContext context) => true;
+
+        public async Task WriteAsync(OutputFormatterWriteContext context)
+        {
+            if (context.Object is null)
+            {
+                return;
+            }
+
+            context.HttpContext.Response.ContentType = "application/json";
+
+            if (context.Object is string json)
+            {
+                await context.HttpContext.Response.WriteAsync(json);
+                return;
+            }
+
+            await _serializer.SerializeAsync(context.HttpContext.Response.Body, context.Object);
+        }
+    }
+}
