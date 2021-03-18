9f6b9ab0f62028739cfcbb4f35f1a65994e1947a
zhilong yang <yzl_200@163.com>
2021-03-18 14:59:29 +0800

    ad jaeger

---
+++ src/Bioworld.Tracing.Jaeger/Tracers/BioworldDefaultTracer.cs
@@ -0,0 +1,18 @@
+﻿using Jaeger;
+using Jaeger.Reporters;
+using Jaeger.Samplers;
+using OpenTracing;
+
+namespace Bioworld.Tracing.Jaeger.Tracers
+{
+    using System.Reflection;
+
+    internal sealed class BioworldDefaultTracer
+    {
+        public static ITracer Create()
+            => new Tracer.Builder(Assembly.GetEntryAssembly().FullName)
+                .WithReporter(new NoopReporter())
+                .WithSampler(new ConstSampler(false))
+                .Build();
+    }
+}
