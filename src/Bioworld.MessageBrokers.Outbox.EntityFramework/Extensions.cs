942e4d409dde0f37aa641a6b5c427138518b72b9
zhilong yang <yzl_200@163.com>
2021-03-19 13:49:15 +0800

    add EF outbox

---
+++ src/Bioworld.MessageBrokers.Outbox.EntityFramework/Extensions.cs
@@ -0,0 +1,22 @@
+﻿namespace Bioworld.MessageBrokers.Outbox.EntityFramework
+{
+    using Internals;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class Extensions
+    {
+        public static IMessageOutboxConfigurator AddEntityFramework<T>(this IMessageOutboxConfigurator configurator)
+            where T : DbContext
+        {
+            var builder = configurator.Builder;
+
+            builder.Services.AddDbContext<T>();
+            builder.Services.AddTransient<IMessageOutbox, EntityFrameworkMessageOutbox<T>>();
+            builder.Services.AddTransient<IMessageOutboxAccessor, EntityFrameworkMessageOutbox<T>>();
+
+            return configurator;
+        }
+    }
+}
+
